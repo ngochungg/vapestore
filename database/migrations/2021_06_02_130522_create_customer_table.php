a2d9285ca62f88577030251e162dace697b3f141
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2021-06-02 21:32:00 +0700

    update full db

---
+++ database/migrations/2021_06_02_130522_create_customer_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('username');
+            $table->string('password');
+            $table->string('address');
+            $table->integer('phone');
+            $table->string('email');
+            $table->string('image');
+            $table->date('birthday');
+            $table->boolean('gender');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
---
+++ database/migrations/2021_06_02_142340_create_order_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->BigInteger('customer_id');
+            $table->string('customer_name');
+            $table->date('order_date');
+            $table->integer('phone');
+            $table->string('email');
+            $table->string('delivery_place');
+            $table->boolean('status');
+            $table->string('delivery_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
