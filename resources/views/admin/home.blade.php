d2cfdc7ce1cc2211cbb6bbdaeb4c329b7baef20c
baodhtv01 <bao.dhtv2978@gmail.com>
2021-06-24 22:18:43 +0700

    dashbord
    áda

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,25 +5,79 @@
 @section('title')
     <title>Home Admin</title>
 @endsection
-
+@section('a_css')
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+@endsection
 @section('content')
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
-    @include('admin.layouts.partials.content_header',['name'=>'admin.home','key'=>'Admin'])
+    @include('admin.layouts.partials.content_header',['name'=>'Dashboard','key'=>' Admin'])
         <!-- /.content-header -->
-
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid">
+                <br>
                 <div class="row">
-                    <div class="col-md-12">
-                        <a href="{{route('categories.create')}}" class="btn btn-success float-right m-2">Add</a>
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{$newOrder}}</h3>
+                                <p>New Orders</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-ios-cart"></i>
+                            </div>
+                            <a href="{{route('order.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
                     </div>
-                    <div class="col-md-12">
-                        home
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>{{$product}}</h3>
+                                <p>Product</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-cube"></i>
+                            </div>
+                            <a href="{{route('product.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
                     </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>{{$user}}</h3>
+                                <p>User Registrations</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="{{route('users.customer')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>{{$complete}}</h3>
 
+                                <p>Completed order</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-checkmark"></i>
+                            </div>
+                            <a href="{{route('order.complete')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
 
                 </div>
                 <!-- /.row -->
